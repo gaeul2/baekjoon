a8446177beaeb1d382b9637ac36cb2bcbf7f9901
gaeul2 <94942563+gaeul2@users.noreply.github.com>
2022-05-17 00:12:41 +0900

    [Bronze III] Title: 알람 시계, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,38 +1,21 @@
-while True:
-    H, M= input().split(" ")
-    H = int(H)
-    M = int(M)
-    if ( H < 0 or H >= 24) and (M < 0 or M >= 60):
-        print('H는 0~23사이의 수로, M은 0~59사이의 수로 입력하세요')
-    elif (M < 0 or M >= 60) :
-        print('M은 0~59사이의 수로 입력하세요')
-    elif ( H < 0 or H >= 24):
-        print('H는 0~23사이의 수로 입력하세요')
-    else:
-        H = (H - 1)
-        M = (M - 45)
-        if M < 0: # M-45가 음수가 될때
-            M = M + 60
-            if H <= 0: #H-1이 0과 같거나 작을때
-                if H == -1:
-                    H = 23
-                    print(H, M)
-                    break
-                else:
-                    print(H, M)
-                    break
-            else:
-                print(H, M)
-                break
+H, M = map(int, input().split())
+
+
+def chang(H,M):
+    min = M -45
+    if H == 0:
+        if min < 0:
+            print(23, min+60)
+        elif min == 0:
+            print(0, 0)
         else:
-            if M == 0: #M이 0과 같을때
-                M = 0
-                print(H+1, M)
-                break
-            else: #M이 0보다 크고 45보다 클때
-                if H == -1:
-                    print(H+1, M)
-                    break
-                else:
-                    print(H+1, M)
-                    break
+            print(0, min)
+    else: 
+        if min < 0:
+            print(H-1, min+60)
+        elif min == 0:
+            print(H, 0)
+        else:
+            print(H, min)
+   
+chang(H,M)
