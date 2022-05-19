b67ecbe45987bd7620739c136c9dedeab81b4eb6
gaeul2 <94942563+gaeul2@users.noreply.github.com>
2022-05-20 00:01:11 +0900

    [Bronze I] Title: 더하기 사이클, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,13 +1,14 @@
-num = int(input())
-check = num #나중에 원래 숫자와 같은지 확인용
-count = 0
+N = int(input())
+num = N
+cnt = 0
+
 while True:
-    a = num // 10 #10으로 나눈 몫이 첫째자리수
-    b = num % 10 #10으로 나눈 나머지가 두번째 자리수
-    c = (a+b)%10 
-    num = (b *10) + c
-    count += 1
-    
-    if num == check:
+    a = N // 10
+    b = N % 10
+    c = (a+b) % 10
+    N = (b * 10) + c
+    cnt += 1
+    if N == num:
         break
-print(count)
+
+print(cnt)
